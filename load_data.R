7bbaaee5140a6c2373eef7264f302e25f1c835a2
Ethan White <ethan@weecology.org>
2022-02-08 09:45:53 -0500

    Move data loading code into separate file

---
+++ load_data.R
@@ -0,0 +1,45 @@
+library(sf)
+
+# Set thresholds
+min_confidence <- 0.4
+min_detections <- 3
+
+#Load data
+raw_data <- load_classifications()
+selected_boxes <- filter_annotations(raw_data)
+colonies <- st_read(
+  "data/colonies.csv",
+  options = c("X_POSSIBLE_NAMES=longitude","Y_POSSIBLE_NAMES=latitude"))
+field_nests <- st_read(
+  "https://raw.githubusercontent.com/weecology/EvergladesWadingBird/main/Nesting/UAV_Flagged_Nest_Coordinates_2021.csv",
+  options = c("X_POSSIBLE_NAMES=long", "Y_POSSIBLE_NAMES=lat"),
+  crs = 4326)
+
+#Predictions
+unzip("data/PredictedBirds.zip", exdir = "data")
+df <- st_read("data/PredictedBirds.shp")
+df$event <- as.Date(df$event,"%m_%d_%Y")
+df$tileset_id <- construct_id(df$site,df$event)
+df <- df %>% filter(score > min_confidence)
+df <- st_transform(df, 4326)
+df <- st_centroid(df)
+year <- sapply(df$event, function(event) str_split(event, "-")[[1]][[1]])
+df <- mutate(df, bird_id = row_number(), year = year)
+
+#Nest predictions
+unzip("data/nest_detections.zip", exdir = "data")
+nestdf <- st_read("data/nest_detections.shp")
+nestdf$Date <- as.Date(nestdf$Date,"%m_%d_%Y")
+nestdf$tileset_id <- construct_id(nestdf$Site,nestdf$Date)
+nestdf <- st_centroid(nestdf)
+nestdf <- st_transform(nestdf,4326)
+selected_indices <- nestdf %>%
+                as.data.frame() %>%
+                filter(score > min_confidence) %>%
+                group_by(Site, target_ind) %>%
+                summarize(n = n()) %>%
+                filter(n >= min_detections) %>%
+                mutate(site_index = paste(Site,target_ind)) 
+nestdf <- nestdf %>%
+  mutate(site_index = paste(Site,target_ind)) %>%
+  inner_join(selected_indices)
