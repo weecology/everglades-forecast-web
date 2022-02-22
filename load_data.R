1c5fb9a67ab70781b84aa954bdcf50e22dfce13a
Ethan White <ethan@weecology.org>
2022-02-22 11:30:44 -0500

    Move random field nest sampling to EvergladesWadingBird repo
    To support consistent links between random samples for evaluating field
    nests this moves the associated code into the EvergladesWadingBird repo
    where consistent sample IDs are created and stored. This app now just
    loads those sample locations and provides them to the user.

--- load_data.R
+++ load_data.R
@@ -12,13 +12,9 @@ colonies <- st_read(
   "data/colonies.csv",
   options = c("X_POSSIBLE_NAMES=longitude","Y_POSSIBLE_NAMES=latitude"))
 field_nests <- st_read(
-  "https://raw.githubusercontent.com/weecology/EvergladesWadingBird/main/Nesting/UAV_Flagged_Nest_Coordinates_2021.csv",
+  "https://raw.githubusercontent.com/weecology/EvergladesWadingBird/main/Nesting/field_nest_sample_locations.csv",
   options = c("X_POSSIBLE_NAMES=long", "Y_POSSIBLE_NAMES=lat"),
   crs = 4326)
-field_nests <- field_nests %>%
-  rename(site = colony) %>%
-  mutate(year = 2021)
-field_nests$site <- trimws(field_nests$site) # There is currently an extra space in at least one value of site
 
 #Predictions
 unzip("data/PredictedBirds.zip", exdir = "data")
@@ -48,26 +44,3 @@ selected_indices <- nestdf %>%
 nestdf <- nestdf %>%
   mutate(site_index = paste(Site,target_ind)) %>%
   inner_join(selected_indices)
-
-# Add random bird locations to field nests
-
-num_field_nests <- nrow(field_nests)
-field_nest_sites <- unique(field_nests$site)
-field_nest_years <- unique(field_nests$year)
-
-set.seed(26) # Keep same nest number and fake nest locations across runs
-
-random_birds <- df %>%
-  filter(site %in% field_nest_sites, year %in% field_nest_years) %>%
-  mutate(real_nest = "No") %>%
-  select(site, year, species = label, real_nest) %>%
-  slice(sample(1:num_field_nests))
-
-field_nests <- field_nests %>%
-  mutate(real_nest = "Yes") %>%
-  select(site, year, species, real_nest) %>%
-  rbind(random_birds) %>%
-  slice(sample(1:n())) %>% # Randomize order to not give clues to real nests
-  mutate(field_nest_id = seq_len(n()))
-
-field_nests
