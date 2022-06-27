eddc120d889596eaaea7614f24e0ade031e735f7
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-06-27 10:57:43 -0700

    selectors are connected

--- prediction_page.R
+++ prediction_page.R
@@ -1,10 +1,15 @@
 predicted_page<-function(df){
   unique(df$tiles)
   renderUI({
-      sidebarLayout(sidebarPanel(leafletOutput("map")),
+      sidebarLayout(sidebarPanel(leafletOutput("map", height=300), width = 2),
+          
       mainPanel(
-        selectInput("prediction_tileset","Event",choices = sort(unique(df$tileset_id)),selected="Joule_03_24_2020"),
+        splitLayout(
+        selectInput("prediction_site","Site",choices = sort(unique(df$site)),selected="Joule"),
+        selectInput("prediction_species","Species",choices = sort(unique(df$label)), multiple = TRUE),
+        ),
         plotOutput("predicted_time_plot"),
+        selectInput("prediction_tileset","Event",choices = sort(unique(df$tileset_id)),selected="Joule_03_24_2020"),
       leafletOutput("sample_prediction_map")))
     })
 }
