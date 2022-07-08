a59b28d0b09b1e6ce8d45100dfbdd13829219eb3
henrykironde <henrykironde@gmail.com>
2022-07-08 03:28:45 -0400

    Add auto update on server script
    The Repo is updated every 20 minutes

---
+++ update_repo.sh
@@ -0,0 +1,8 @@
+changed=0
+git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+if [ $changed = 1 ]; then
+    git reset --hard origin/main && git pull
+    echo "Updated $(date)"
+else
+    echo "Up-to-date $(date)"
+fi
