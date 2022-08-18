f5fd9ee49b14647a975b0f05bb86570145df0e74
henrykironde <henrykironde@gmail.com>
2022-08-18 15:42:45 -0400

    Refomat code

--- functions.R
+++ functions.R
@@ -13,7 +13,7 @@ create_map <- function(colonies) {
   m <-
     leaflet(data = colonies) %>%
     addTiles() %>%
-    addMarkers(popup = ~site)
+    addMarkers(popup = ~ site)
   return(renderLeaflet(m))
 }
 
@@ -55,32 +55,26 @@ filter_annotations <- function(raw_data) {
 
   # !!Temp hotfix!!! until events are seperated from dates
   selected_boxes$event[selected_boxes$event %in% "03112020"] <-
-    gsub(
-      x = selected_boxes$event[selected_boxes$event %in% "03112020"],
-      pattern = "03112020",
-      replacement = "03_11_2020"
-    )
+    gsub(x = selected_boxes$event[selected_boxes$event %in% "03112020"],
+         pattern = "03112020",
+         replacement = "03_11_2020")
   selected_boxes$event <- as.Date(selected_boxes$event, "%m_%d_%Y")
   selected_boxes$tileset_id <-
     construct_id(selected_boxes$site, selected_boxes$event)
 
   # get unique boxes among observers
-
   return(selected_boxes)
 }
 
 plot_annotations <- function(selected_boxes, MAPBOX_ACCESS_TOKEN) {
-  pal <- colorFactor(
-    palette = "Dark2",
-    domain = selected_boxes$species
-  )
+  pal <- colorFactor(palette = "Dark2",
+                     domain = selected_boxes$species)
 
   selected_centroids <- st_transform(selected_boxes, 4326)
 
   # Create mapbox tileset
   mapbox_tileset <- unique(selected_centroids$tileset_id)
   mapbox_tileset <- paste("bweinstein.", mapbox_tileset, sep = "")
-
   m <- leaflet(data = selected_centroids) %>%
     addProviderTiles(
       "MapBox",
@@ -137,42 +131,39 @@ time_predictions <-
         df %>%
         filter(site == select_site) %>%
         group_by(site, event, year) %>%
-        summarize(
-          n =
-            n()
-        )
+        summarize(n =
+                    n())
       ggplot(g, aes(x = event, y = n)) +
         geom_point(aes(color = g$event == selected_event), size = 3.5) +
         geom_line() +
         labs(y = "Detected Birds", x = "Date") +
         theme(text = element_text(size = 20)) +
-        facet_wrap(nrow = 1, ~year, scales = "free_x") +
+        facet_wrap(nrow = 1, ~ year, scales = "free_x") +
         scale_color_manual(guide = "none", values = c("black", "red"))
     } else {
       g <-
         df %>%
         filter(site == select_site, label %in% species) %>%
         group_by(site, event, year, label) %>%
-        summarize(
-          n = n()
-        )
+        summarize(n = n())
       ggplot(g, aes(x = event, y = n)) +
-        geom_point(aes(color = g$event == selected_event, shape=label), size = 3) +
+        geom_point(aes(
+          color = g$event == selected_event,
+          shape = label
+        ), size = 3) +
         geom_line(aes(linetype = label)) +
         theme(text = element_text(size = 20)) +
         labs(x = "Date", color = "Species", y = "Detected Birds") +
-        facet_wrap(nrow = 1, ~year, scales = "free_x") +
+        facet_wrap(nrow = 1, ~ year, scales = "free_x") +
         scale_color_manual(guide = "none", values = c("black", "red")) +
-        scale_shape(guide="none") + scale_linetype(guide="none")
+        scale_shape(guide = "none") + scale_linetype(guide = "none")
     }
   }
 
 species_colors <- colorFactor(
-  palette = c(
-    "yellow", "blue",
-    "#ff007f", "brown",
-    "purple", "white"
-  ),
+  palette = c("yellow", "blue",
+              "#ff007f", "brown",
+              "purple", "white"),
   domain = c(
     "Great Egret",
     "Great Blue Heron",
