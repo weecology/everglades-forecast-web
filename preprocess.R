93a46f3ee61bc8ed5e8fa58dc5d7aaf2c07ce897
henrykironde <henrykironde@gmail.com>
2024-05-05 00:49:59 -0400

    Update with 2023 data and correct field names to match the data.

--- preprocess.R
+++ preprocess.R
@@ -3,11 +3,14 @@ library(sf)
 library(dplyr)
 source("functions.R")
 
+
+# Set thresholds
+min_confidence <- 0.4
 ### Preprocess .shp once to improve load time. This code needs to be integrated upstream.
 unzip("data/PredictedBirds.zip", exdir = "data")
 df <- st_read("data/PredictedBirds.shp")
 df <- df %>% filter(
-  site %in% c(
+  Site %in% c(
     "6thBridge",
     "CypressCity",
     "Jerrod",
@@ -18,11 +21,12 @@ df <- df %>% filter(
   )
 ) %>%
   select(-xmin, -ymin, -xmax, -ymax)
-df$event <- as.Date(df$event, "%m_%d_%Y")
-df$tileset_id <- construct_id(df$site, df$event)
+df$event <- as.Date(df$Date, "%m_%d_%Y")
+df$tileset_id <- construct_id(df$Site, df$event)
 df <- df %>% filter(score > min_confidence)
 df <- st_transform(df, 4326)
-year <- sapply(df$event, function(event) str_split(event, "-")[[1]][[1]])
-df <- mutate(df, bird_id = row_number(), year = year)
+#year <- sapply(df$event, function(event) str_split(event, "-")[[1]][[1]])
+df <- mutate(df, bird_id = row_number()) %>% rename(year=Year, site=Site)
 
 write_sf(df, "data/PredictedBirds.shp")
+
